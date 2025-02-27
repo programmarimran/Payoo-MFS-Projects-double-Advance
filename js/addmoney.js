e299875fbd045c4d9115c5112fb3f2746786f8b2
programmarimran <programmarimran@gmail.com>
2025-02-27 17:59:25 +0600

    added a add money is complite

--- js/addmoney.js
+++ js/addmoney.js
@@ -1,23 +1,47 @@
 document.getElementById("btn-add-money")
 .addEventListener("click",function(event){
-    // event.preventDefault();
+    event.preventDefault();
     const selectBank=document.getElementById("add-select-bank").value;
     const accountNumber=document.getElementById("add-money-account-number").value;
     const amount=getConvertedInputValue("add-money-enter-amount");
     const pin=getConvertedInputValue("add-money-pin-number");
     const mainBalance=getConvertedInnerText("main-balance")
-   if(selectBank!=="Bkash" && selectBank!=="Nagad" && selectBank!=="DBBL"){
+
+    //*************************************************************** */
+   if(selectBank !== "Bkash" && selectBank !== "Nagad" && selectBank !== "DBBL"){
     alert("please select Bank")
+    return
    }
    else if(accountNumber.length!==11 || accountNumber[0]!=="0" || accountNumber[1]!=="1"){
     alert("please valid Account number")
+    return
    }
    else if(pin!==1234){
     alert("Invalid pin")
+    return
    }
-   else if(amount<0){
+   else if(amount<=0 || isNaN(amount)){
     alert("please positive amount")
     return;
    }
-   
+   else{
+    const sum=mainBalance+amount;
+    document.getElementById("main-balance").innerText=sum;
+   }
+   //**************************************************************** */
+   const historyContainer=document.getElementById("history-container");
+   const div=document.createElement("div")
+   div.innerHTML=`
+   <div class="flex justify-between items-center bg-white p-3 rounded-xl">
+                <div class="flex justify-between items-center">
+                   <div class="bg-[#F4F5F7] h-10 w-10 rounded-full flex justify-center items-center"> <img class="w-1/2" src="./assets/wallet1.png" alt=""></div>
+                    <div class="ml-2">
+                        <h1 class="font-semibold text-base text-[#666768]">Added ${amount} tk</h1>
+                        <p class="font-normal text-[12px] text-[#666768]">from ${accountNumber}</p>
+                    </div>
+                </div>
+                <i class="fa-solid fa-ellipsis-vertical"></i>
+            </div>
+   `
+   historyContainer.appendChild(div);
 })
